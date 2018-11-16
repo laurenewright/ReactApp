5fbc635cf2ad47b34a512276f774218621545528
lauren wright <wright.lauren20@gmail.com>
2018-11-16 11:46:30 -0500

    fixed some indenting

--- src/App.js
+++ src/App.js
@@ -16,25 +16,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <BrowserRouter>
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <ul className="main-nav">
-              <li><NavLink to="/ProfileCharacter">Characters</NavLink></li>
-              <li><NavLink to="/ProfileHouse">Houses</NavLink></li>
-              <li><NavLink to="/ProfileBook">Books</NavLink></li>
-            </ul>
-        </header>
-              <Switch className = "main-page">
-                <Route path="/ProfileCharacter" component={profileCharacter}/>
-                <Route path="/ProfileBook" component={profileBook} />
-                <Route path="/ProfileHouse" component={profileHouse} />
-            </Switch>
-
-        </div>
+        <BrowserRouter>
+          <div>
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+                <ul className="main-nav">
+                  <li><NavLink to="/ProfileCharacter">Characters</NavLink></li>
+                  <li><NavLink to="/ProfileHouse">Houses</NavLink></li>
+                  <li><NavLink to="/ProfileBook">Books</NavLink></li>
+                </ul>
+            </header>
+                <Switch className = "main-page">
+                  <Route path="/ProfileCharacter" component={profileCharacter}/>
+                  <Route path="/ProfileBook" component={profileBook} />
+                  <Route path="/ProfileHouse" component={profileHouse} />
+                </Switch>
+          </div>
         </BrowserRouter>
-
       </div>
     );
   }
